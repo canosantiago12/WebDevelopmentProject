38cb39a4a7a035aefd95e3dfe74cfc3e869add6b
Daniel David <danielalejandrods.2000@gmail.com>
2021-11-27 16:42:37 -0600

    users login and signup

---
+++ backend/controllers/authController.js
@@ -0,0 +1,62 @@
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
+const db = require("../models");
+require('dotenv').config();
+
+const User = db.user;
+
+exports.signup = (req, res) => {
+  const user = new User({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8)
+  });
+
+  user.save(err => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+
+    res.send({ message: "User was registered successfully!" });
+  });
+};
+
+exports.signin = (req, res) => {
+  User.findOne({
+    userName: req.body.userName
+  })
+  .exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+
+    if (!user) {
+      return res.status(404).send({ message: "User Not found." });
+    }
+
+    var passwordIsValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: "Invalid Password!"
+      });
+    }
+
+    var token = jwt.sign({ id: user.id }, process.env.SECRET, {
+      expiresIn: 86400
+    });
+
+    res.status(200).send({
+      id: user._id,
+      userName: user.userName,
+      email: user.email,
+      accessToken: token
+    });
+  });
+};
