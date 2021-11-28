c09ee0a6115b4322d389e263b8bb3208fc2401aa
Santiago Cano <canosantiagoh@gmail.com>
2021-11-28 17:17:47 -0600

    Profile page almost done

--- frontend/src/pages/PageStyle.js
+++ frontend/src/pages/PageStyle.js
@@ -13,7 +13,7 @@ export const LandingPage = styled.div`
     display: flex;
     align-items: center;
     width: 100%;
-    height: 90vh;
+    height: 89.7vh;
     background: url(${ imgLanding }) no-repeat center;
     animation: ${ backgroundMovingAnimation } 30s ease infinite;
     position: relative;
@@ -24,7 +24,7 @@ export const LoginPage = styled.div`
     align-items: center;
     justify-content: center;
     width: 100%;
-    height: 90vh;
+    height: 89.7vh;
     position: relative;
     background: url(${ imgLanding }) no-repeat center;
     animation: ${ backgroundMovingAnimation } 30s ease infinite;
@@ -34,13 +34,29 @@ export const RegisterPage = styled.div`
     display: flex;
     align-items: center;
     width: 100%;
-    height: 90vh;
+    height: 89.7vh;
     position: relative;
     background: url(${ imgLanding }) no-repeat center;
     animation: ${ backgroundMovingAnimation } 30s ease infinite;
 `
 
+const gradient = keyframes`
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+`
+
 export const ProfilePage = styled.div`
     width: 100%;
-    height: 90vh;
+    height: 95vh;
+    /* background: linear-gradient(-45deg, #191A1A, #1D1E1E, #343535, #464747);
+    background-size: 400% 400%;
+    animation: ${ gradient } 15s ease infinite; */
+    background: #000000;
 `
