59b53345d98f7a91c3959f938200bd16857f5e27
Daniel David <danielalejandrods.2000@gmail.com>
2021-11-28 20:58:29 -0600

    fetch users

--- frontend/src/services/auth-header.js
+++ frontend/src/services/auth-header.js
@@ -2,7 +2,10 @@ export default function authHeader() {
     const user = JSON.parse(localStorage.getItem('user'));
   
     if (user && user.accessToken) {
-        return { 'x-access-token': user.accessToken };
+        return { 
+          'Access-Control-Allow-Headers': 'x-access-token',
+          'x-access-token': user.accessToken 
+        };
     } else {
         return {};
     }
