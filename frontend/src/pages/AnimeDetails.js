d9707df2533586076d139ac3805ccb8fce4a90aa
Santiago Cano <canosantiagoh@gmail.com>
2021-11-29 23:29:46 -0600

    Initial design for anime details page

--- frontend/src/pages/AnimeDetails.js
+++ frontend/src/pages/AnimeDetails.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { DetailsPage } from './PageStyle';
+import { GiSeahorse } from "react-icons/gi";
+
+import exp from '../assets/images/MHA.png'
 
 const AnimeDetails = ({ currentUser }) => {
     return (
@@ -8,12 +11,26 @@ const AnimeDetails = ({ currentUser }) => {
                 <div className="main-content">
                     <div className="container-fluid px-0 px-sm-3">
                         <dv className="row">
-                            <div className="col-md-4 mx-auto">
+                            <div className="col-md-8 mx-auto">
                                 <div className="animeDetailsContainer">
-                                    <h1 className="text-start">Anime title</h1>
-                                    <hr />
-                                    <div className="animeDetails">
-
+                                    <h1 className="text-start ps-4">My Hero Academia</h1>
+                                    <div className="divider">
+                                    </div>
+                                    <div className="animeDetails d-flex pt-3 ps-4">
+                                        <div className="d-flex flex-column align-items-center">
+                                        <img src={exp}/>
+                                        <p className="pt-2"><span>Score: </span><GiSeahorse size="3rem" color="#FFFFFF"/></p>
+                                        </div>
+                                        <div className="animeDetailsText ps-4">
+                                            <p><span>Title: </span></p>
+                                            <p><span>Type: </span></p>
+                                            <p><span>Runtime: </span></p>
+                                            <p><span>Airing: </span></p>
+                                            <p><span>Content rating: </span></p>
+                                            <p><span>Start date: </span></p>
+                                            <p><span>End date: </span></p>
+                                            <p><span>Synopsis: </span><br /></p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
